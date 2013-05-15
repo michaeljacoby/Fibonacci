e6e7188d1e57883ba90eccd2a5673d0a91f7916e
Michael <michael@michael-Studio-XPS-1640.(none)>
2013-05-14 20:02:44 -0500

    Added a fibonacci checker

--- lib/fibon.rb
+++ lib/fibon.rb
@@ -4,4 +4,30 @@ module Fibon
   def self.place(a)
     @x = ((1 / Math::sqrt(5))*(((1+Math::sqrt(5))/2)**(a))).round
   end
+
+  def self.check(a)
+    value = false
+
+    firstcheck = (5*(a**2))+4
+    secondcheck = (5*(a**2))-4
+
+    if (self.isPerfectSquare(firstcheck) == true) or (self.isPerfectSquare(secondcheck) == true)
+      value = true
+    else
+      
+    end
+    value
+  end
+
+  def self.isPerfectSquare(x)
+    value = false
+    if Math::sqrt(x) == Math::sqrt(x).round
+      value = true
+    else
+    
+    end
+    
+    value
+  end
+
 end
---
+++ lib/main.rb
@@ -0,0 +1,9 @@
+require './fibon'
+
+
+ x = 0
+while x < 10
+puts Fibon.check(x)
+
+x = x+1
+end
